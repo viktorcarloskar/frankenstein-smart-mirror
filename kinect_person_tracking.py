59b6f5561a1943ba554c4c5a20f3e16be97c5397
Viktor Gustafsson <viktor@vigu.se>
2016-04-28 14:22:18 +0200

    Installed flask-websockets and got it running

--- kinect_person_tracking.py
+++ kinect_person_tracking.py
@@ -4,8 +4,16 @@
 from __future__ import print_function
 import time
 from flask import Flask
+from flask_sockets import Sockets
 
 app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
 
 @app.route("/")
 def hello():
@@ -13,5 +21,8 @@ def hello():
 
 if __name__ == "__main__":
     print('oh hello')
-    #time.sleep(5)
-    app.run(host='127.0.0.1', port=5000)
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
+    #app.run(host='127.0.0.1', port=5000)
